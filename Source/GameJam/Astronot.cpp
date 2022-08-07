2b21b9afd1c56fbf54f14037512341335c30f60d
Emin Yahya Arabacı <Pro_yahya_lol@hotmail.com>
2022-08-07 22:56:00 +0300

    Reborn

---
+++ Source/GameJam/Astronot.cpp
@@ -0,0 +1,216 @@
+
+#include "Astronot.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Components/SphereComponent.h"
+#include "Components/WidgetComponent.h"
+#include "GameJam/Enemies/Enemy.h"
+#include "GameJam/Enemies/Commander.h"
+#include "StoneProjectile.h"
+
+// Sets default values
+AAstronot::AAstronot() :
+	bForBlockMovement(false),
+	DefaultRuningSpeed(0.f),
+	CharacterActifSkills(ECharacterSkills::ECS_ThrowRock),
+	bCanThrowStone(true),
+	TimeBetweenThrowingStone(5.f)
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm Component"));
+	SpringArmComp->SetupAttachment(RootComponent);
+
+	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+	CameraComp->SetupAttachment(SpringArmComp);
+
+	AreaSphere = CreateDefaultSubobject<USphereComponent>(TEXT("Spehere component for Player"));
+	AreaSphere->SetupAttachment(RootComponent);
+
+	AreaSphereForAssimillitaion = CreateDefaultSubobject<USphereComponent>(TEXT("Sphere comp for assilmilition widget to pop up "));
+	AreaSphereForAssimillitaion->SetupAttachment(RootComponent);
+
+	ProjectileSpawnPoint = CreateDefaultSubobject<USceneComponent>(TEXT("Spawn point of Projectile"));
+	ProjectileSpawnPoint->SetupAttachment(RootComponent);
+
+
+
+}
+
+// Called when the game starts or when spawned
+void AAstronot::BeginPlay()
+{
+	;
+	Super::BeginPlay();
+
+	if (OverrideInitialSkill)
+		SetCharacterActiveSkill(OverrideSkill);
+
+	bEnemyOverlappedWithAssimilation = false;
+
+	DefaultRuningSpeed = GetCharacterMovement()->MaxWalkSpeed;
+
+	AreaSphereForAssimillitaion->OnComponentBeginOverlap.AddDynamic(this, &AAstronot::OnAssimillitionSphereOverlapped);
+	AreaSphereForAssimillitaion->OnComponentEndOverlap.AddDynamic(this, &AAstronot::OnAssimillitionSphereEndOverlapped);
+
+	AreaSphere->OnComponentBeginOverlap.AddDynamic(this, &AAstronot::OnInfoSphereOverlapped);
+	AreaSphere->OnComponentEndOverlap.AddDynamic(this, &AAstronot::OnInfoSphereEndOverlapped);
+
+}
+
+void AAstronot::GoForwardFunction(float Value)
+{
+	if (!bForBlockMovement)
+	{
+		AddMovementInput(FVector(1, 0, 0), Value);
+	}
+}
+
+void AAstronot::GoRightFunction(float Value)
+{
+	if (!bForBlockMovement)
+	{
+		AddMovementInput(FVector(0, 1, 0), Value);
+	}
+
+
+}
+
+void AAstronot::Runing()
+{
+	GetCharacterMovement()->MaxWalkSpeed = SpeedWhileRuning;
+}
+
+void AAstronot::ReleasedRuningKeyFunction()
+{
+	GetCharacterMovement()->MaxWalkSpeed = DefaultRuningSpeed;
+}
+
+
+void AAstronot::OnInfoSphereOverlapped(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepHitResult)
+{
+
+	ACommander* CommanderRef = Cast<ACommander>(OtherActor);
+
+	if (CommanderRef)
+	{
+		CommanderRef->GetInfoWidget()->SetVisibility(true);
+	}
+}
+
+void AAstronot::OnInfoSphereEndOverlapped(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	ACommander* CommanderRef = Cast<ACommander>(OtherActor);
+
+	if (CommanderRef)
+	{
+		CommanderRef->GetInfoWidget()->SetVisibility(false);
+	}
+}
+
+void AAstronot::OnAssimillitionSphereOverlapped(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepHitResult)
+{
+
+	ACommander* CommanderRef = Cast<ACommander>(OtherActor);
+
+	if (CommanderRef)
+	{
+		CommanderRef->GetAssimilitionWidget()->SetVisibility(true);
+		CommanderRef->GetInfoWidget()->SetVisibility(false);
+	}
+}
+
+void AAstronot::OnAssimillitionSphereEndOverlapped(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+
+	ACommander* CommanderRef = Cast<ACommander>(OtherActor);
+
+	if (CommanderRef)
+	{
+		CommanderRef->GetAssimilitionWidget()->SetVisibility(false);
+		CommanderRef->GetInfoWidget()->SetVisibility(true);
+		bEnemyOverlappedWithAssimilation = false;
+	}
+}
+
+void AAstronot::SetTimerBetweenThrowRock()
+{
+	bCanThrowStone = true;
+}
+
+
+// Called every frame
+void AAstronot::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void AAstronot::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis(FName("Forward"), this, &AAstronot::GoForwardFunction);
+	PlayerInputComponent->BindAxis(FName("Right"), this, &AAstronot::GoRightFunction);
+
+	PlayerInputComponent->BindAction(FName("Runing"), EInputEvent::IE_Pressed, this, &AAstronot::Runing);
+	PlayerInputComponent->BindAction(FName("Runing"), EInputEvent::IE_Released, this, &AAstronot::ReleasedRuningKeyFunction);
+
+	PlayerInputComponent->BindAction(FName("UseSkill"), EInputEvent::IE_Pressed, this, &AAstronot::UseSkill);
+
+}
+
+void AAstronot::UseSkill()
+{
+
+	switch (CharacterActifSkills)
+	{
+	case ECharacterSkills::ECS_ThrowRock:
+		if (bCanThrowStone)
+		{
+			SpawnProjectileToWorld();
+			bCanThrowStone = false;
+			GetWorld()->GetTimerManager().SetTimer(ThrowStoneTimer, this, &AAstronot::SetTimerBetweenThrowRock, TimeBetweenThrowingStone, false);
+		}
+		break;
+	case ECharacterSkills::ECS_Teleport:
+		Teleport();
+		break;
+
+	case ECharacterSkills::ECS_Hook:
+		Hook();
+		break;
+
+	case ECharacterSkills::ECS_Stone:
+		Stone();
+		break;
+	case ECharacterSkills::ECS_FireResistance:
+		FireResistance();
+		break;
+
+	case ECharacterSkills::ECS_MeltIce:
+		MeltIce();
+		break;
+
+	case ECharacterSkills::ECS_Max:
+		break;
+	default:
+		break;
+	}
+}
+
+void AAstronot::SpawnProjectileToWorld()
+{
+	AStoneProjectile* ProjectileRef = GetWorld()->SpawnActor<AStoneProjectile>(StoneProjectileClass,
+		ProjectileSpawnPoint->GetComponentLocation(),
+		ProjectileSpawnPoint->GetComponentRotation());
+
+	ProjectileRef->SetOwner(this);
+}
+
+void AAstronot::SetCharacterActiveSkill(ECharacterSkills Skill)
+{
+	CharacterActifSkills = Skill;
+}
---
+++ Source/GameJam/Astronot.h
@@ -0,0 +1,174 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Astronot.generated.h"
+
+
+UENUM()
+enum class ECharacterSkills
+{
+	ECS_Ghost UMETA(DisplayName = "Ghost"),
+	ECS_ThrowRock UMETA(DisplayName = "ThrowRock"),
+	ECS_Teleport UMETA(DisplayName = "Teleport"),
+	ECS_Dash UMETA(DisplayName = "Dash"),
+	ECS_Hook UMETA(DisplayName = "Hook"),
+	ECS_Stone UMETA(DisplayName = "Stone"),
+	ECS_Assimilation UMETA(DisplayName = "Assimilation"),
+	ECS_FireResistance UMETA(DisplayName = "Fire Resistance"),
+	ECS_MeltIce UMETA(DisplayName = "Melt Ice"),
+
+	ECS_Max UMETA(DisplayName = "Max"),
+
+};
+
+UCLASS()
+
+class GAMEJAM_API AAstronot : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AAstronot();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	void GoForwardFunction(float Value);
+
+	void GoRightFunction(float Value);
+
+	void Runing();
+
+	void ReleasedRuningKeyFunction();
+
+
+	UFUNCTION()
+		void OnInfoSphereOverlapped(UPrimitiveComponent* OverlappedComp,
+			AActor* OtherActor,
+			UPrimitiveComponent* OtherComponent,
+			int32 OtherBodyIndex,
+			bool bFromSweep,
+			const FHitResult& SweepHitResult);
+
+
+	UFUNCTION()
+		void OnInfoSphereEndOverlapped(UPrimitiveComponent* OverlappedComp,
+			AActor* OtherActor,
+			UPrimitiveComponent* OtherComp,
+			int32 OtherBodyIndex);
+
+
+	UFUNCTION()
+		void OnAssimillitionSphereOverlapped(UPrimitiveComponent* OverlappedComp,
+			AActor* OtherActor,
+			UPrimitiveComponent* OtherComponent,
+			int32 OtherBodyIndex,
+			bool bFromSweep,
+			const FHitResult& SweepHitResult);
+
+
+	UFUNCTION()
+		void OnAssimillitionSphereEndOverlapped(UPrimitiveComponent* OverlappedComp,
+			AActor* OtherActor,
+			UPrimitiveComponent* OtherComp,
+			int32 OtherBodyIndex);
+
+	void SetTimerBetweenThrowRock();
+
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+
+	bool bForBlockMovement;
+
+	/*store the default walk speed*/
+	float DefaultRuningSpeed;
+
+	//store the speed when you pressed shift key
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Component", meta = (AllowPrivateAccess = "true"))
+		float SpeedWhileRuning;
+
+	UPROPERTY(VisibleAnywhere, Category = "Component")
+		class USpringArmComponent* SpringArmComp = nullptr;
+
+	UPROPERTY(VisibleAnywhere, Category = "Component")
+		class UCameraComponent* CameraComp = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Component", meta = (AllowPrivateAccess = "true"))
+		ECharacterSkills CharacterActifSkills;
+
+	/*attaced to RootComponent*/
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Component", meta = (AllowPrivateAccess = "true"))
+		class USphereComponent* AreaSphere;
+
+	/*attaced to RootComponent*/
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Component", meta = (AllowPrivateAccess = "true"))
+		class USphereComponent* AreaSphereForAssimillitaion;
+
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true", Category = "Abilities|Init", InlineEditConditionToggle))
+		bool OverrideInitialSkill = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true", Category = "Abilities|Init", EditCondition = "OverrideInitialSkill"))
+		TEnumAsByte<ECharacterSkills> OverrideSkill;
+
+
+
+	bool bEnemyOverlappedWithAssimilation;
+
+	void UseSkill();
+
+	void SpawnProjectileToWorld();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Component", meta = (AllowPrivateAccess = "true"))
+		TSubclassOf<class AStoneProjectile> StoneProjectileClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Component", meta = (AllowPrivateAccess = "true"))
+		USceneComponent* ProjectileSpawnPoint = nullptr;
+
+	bool bCanThrowStone;
+
+	FTimerHandle ThrowStoneTimer;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Component", meta = (AllowPrivateAccess = "true"))
+		float TimeBetweenThrowingStone;
+
+
+
+
+	/*For Getter and setter functions*/
+public:
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+		void Teleport();
+
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+		void Hook();
+
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+		void Stone();
+
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+		void FireResistance();
+
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+		void MeltIce();
+	FORCEINLINE USpringArmComponent* GetSpringArmComp() const { return SpringArmComp; }
+
+	FORCEINLINE UCameraComponent* GetCameraComp() const { return CameraComp; }
+
+	FORCEINLINE ECharacterSkills GetCharacterActiveSkill() const { return CharacterActifSkills; }
+
+	void SetCharacterActiveSkill(ECharacterSkills Skill);
+};
